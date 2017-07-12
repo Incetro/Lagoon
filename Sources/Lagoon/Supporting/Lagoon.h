94a1f8afb59763a4755d7f60d0c516ef1d1a89a0
incetro <incetro@ya.ru>
2017-07-12 15:42:19 +0300

    Minor changes

--- Sources/Lagoon/Supporting/Lagoon.h
+++ Sources/Lagoon/Supporting/Lagoon.h
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for Lagoon iOS.
-FOUNDATION_EXPORT double Lagoon_iOSVersionNumber;
+//! Project version number for Lagoon
+FOUNDATION_EXPORT double LagoonVersionNumber;
 
-//! Project version string for Lagoon iOS.
-FOUNDATION_EXPORT const unsigned char Lagoon_iOSVersionString[];
+//! Project version string for Lagoon
+FOUNDATION_EXPORT const unsigned char LagoonVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Lagoon_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Lagoon/PublicHeader.h>
 
 
