07a0e08e30b0dc73039297069b51f014ef3b61fc
TraxxasWyls <savinov.dsta@gmail.com>
2023-11-01 19:34:57 +0300

    Add package support

--- Package.swift
+++ Package.swift
@@ -1,15 +1,27 @@
-//
-//  Package.swift
-//  Lagoon
-//
-//  Created by incetro on 12/07/2017.
-//
-//
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Lagoon",
-    dependencies : [],
-    exclude: ["Tests"]
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Lagoon",
+            targets: ["Lagoon"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Lagoon"),
+        .testTarget(
+            name: "LagoonTests",
+            dependencies: ["Lagoon"]),
+    ]
 )
